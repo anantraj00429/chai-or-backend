b1cfc1fb7df5a6568fef0bfdb865ccac62f32e1f
anantraj0429 <anantraj0429@gmail.com>
2024-06-07 14:58:06 +0530

    changes commit

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
+    credentials: true,
   })
 );
 
@@ -14,5 +15,11 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+//router import
+import userRouter from "./routes/user.routes.js";
+
+//routes declaration
+app.use("/api/v1/users", userRouter);
 
 export { app };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running at PORT : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!!", err);
+  });
 
 // import express from "express"
 // import connectDB from "./db";
